8376b301366174ece284dde0043e9b50bfe522b7
Lucas Massarico <lucasmassarico1@gmail.com>
2024-05-01 23:10:51 -0300

    Update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,85 @@
-import { FooterContainer } from "./style";
+import {
+    FooterContainer,
+    FooterColumn,
+    ColumnTitle,
+    ColumnItem,
+    ColumnItemLink,
+    ColumnItemWithIcon,
+} from "./style";
+
+import {
+    InstagramLogo,
+    FacebookLogo,
+    WhatsappLogo,
+    Phone,
+} from "phosphor-react";
 
 export const Footer = () => {
     return (
         <FooterContainer>
-            <h1>Footer</h1>
+            <FooterColumn>
+                <ColumnTitle>Categorias</ColumnTitle>
+                <ColumnItemLink href="/teste">Teste e teste</ColumnItemLink>
+            </FooterColumn>
+            <FooterColumn>
+                <ColumnTitle>Sobre Nós</ColumnTitle>
+                <ColumnItemLink href="/sobrenos">Sobre Nós</ColumnItemLink>
+            </FooterColumn>
+            <FooterColumn>
+                <ColumnTitle>Redes Sociais</ColumnTitle>
+                <ColumnItemWithIcon
+                    href="https://www.instagram.com"
+                    target="_blank"
+                >
+                    <InstagramLogo size={32} />
+                    /antiquario
+                </ColumnItemWithIcon>
+                <ColumnItemWithIcon
+                    href="https://www.facebook.com"
+                    target="_blank"
+                >
+                    <FacebookLogo size={32} />
+                    /antiquario
+                </ColumnItemWithIcon>
+            </FooterColumn>
+            <FooterColumn>
+                <ColumnTitle>Atendimento</ColumnTitle>
+                <ColumnItem>
+                    <p>Horário de atendimento:</p>
+                    <p>08:00 às 18:00 -</p>
+                    <p>Segunda a Sexta,</p>
+                    <p>horário de Brasília</p>
+                </ColumnItem>
+                <br />
+                <ColumnItem>
+                    <b>
+                        <p>Endereço:</p>
+                    </b>
+                    <p>Rua Ficitia, 1322 -</p>
+                    <p>Jardim Ficticio</p>
+                    <p>São Manuel/SP - CEP: 18650-000</p>
+                </ColumnItem>
+                <br />
+                <ColumnItem>
+                    <b>
+                        <span>
+                            <Phone />
+                            Telefone
+                        </span>
+                        <span>
+                            {" / "}
+                            <WhatsappLogo />
+                            Whatsapp:
+                        </span>
+                    </b>
+                    <p>+55 (14) 99197-4654</p>
+                </ColumnItem>
+                <br />
+                <ColumnItem>
+                    <b>E-mail:</b>
+                    <p>faleconosco@antiquario.com.br</p>
+                </ColumnItem>
+            </FooterColumn>
         </FooterContainer>
     );
 };
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,8 +1,41 @@
 import { styled } from "@/styles";
+import Link from "next/link";
 
 export const FooterContainer = styled("div", {
-    display: "flex",
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gap: "$4",
     width: "100%",
+    minHeight: "10rem",
     backgroundColor: "$lightOrange",
-    marginTop: "$16",
+    color: "$white",
+    margin: "$16 0 $4 0",
+});
+
+export const FooterColumn = styled("div", {
+    margin: "0 auto",
+    padding: "$2",
+});
+
+export const ColumnTitle = styled("h3", {
+    fontWeight: "$bold",
+    fontSize: "$md",
+});
+
+export const ColumnItem = styled("span", {
+    fontSize: "$xs",
+});
+
+export const ColumnItemLink = styled(Link, {
+    textDecoration: "none",
+    color: "$white",
+    fontSize: "$sm",
+});
+
+export const ColumnItemWithIcon = styled(Link, {
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+    color: "$white",
+    fontSize: "$sm",
 });
